37e596cf9656b9838e035d93176d03b476817032
Guilherme Santos <sntguilherme@gmail.com>
2022-02-16 10:58:49 +0100

    Removing out and npm deploy script

--- out/_next/static/TysE6YWrIUsz2j80oEYF1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/526-3980c73d5d63c1d0.js","static/chunks/pages/index-1d0cb02e0ee26e2e.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
